d516633d45cbf1928dd362e5276759bb63c733f8
Adarsh Singh <adarshsingh452002@gmail.com>
2024-01-03 11:22:44 +0530

    added data and database

--- server/index.js
+++ server/index.js
@@ -16,3 +16,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL,{
+})
+.then(async ()=>{
+    app.listen(PORT,()=>console.log(`Server Port: ${PORT}`))
+})
+.catch((error)=>console.log(`${error} did not connect`))
